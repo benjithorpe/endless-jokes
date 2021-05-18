d58224fe4ff25232dbb40a388920d9fecdb78404
benjithorpe <benjaminthorpe19@gmail.com>
2021-05-18 18:57:29 +0000

    Refactor code, format numbers

--- main.js
+++ main.js
@@ -1,34 +1,60 @@
 const dataContainer = document.getElementById('data');
 const countryName = document.getElementById('countryName');
 const activeCases = document.getElementById('activeCases');
+const totalDeaths = document.getElementById('deaths');
 const totalCases = document.getElementById('totalCases');
 const critical = document.getElementById('critical');
 const tested = document.getElementById('tested');
-const totalDeaths = document.getElementById('deaths');
-const totalRecovered = document.getElementById('recovered');
-const deathRatio = document.getElementById('deathRatio');
-const recoveryRatio = document.getElementById('recoveryRatio');
 const searchBtn = document.querySelector("button[type='search']");
 const searchBar = document.querySelector("input[type='search']");
+const deathRatio = document.getElementById('deathRatio');
+const recoveryRatio = document.getElementById('recoveryRatio');
+const totalRecovered = document.getElementById('recovered');
+let errorParagraph = document.getElementById('error');
 const latestDataAPI = "https://api.quarantine.country/api/v1/summary/latest";
+// let countryAPI = "https://api.quarantine.country/api/v1/summary/region?region=florida&sub_areas=1"
 
 
 fetch(latestDataAPI)
   .then((response) => response.json())
-  .then(function(jsonResponse){
-    activeCases.innerHTML = jsonResponse['data']['summary']['active_cases'];
-    totalCases.innerHTML = jsonResponse['data']['summary']['total_cases'];
-    critical.innerHTML = jsonResponse['data']['summary']['critical'];
-    totalDeaths.innerHTML = jsonResponse['data']['summary']['deaths'];
-    deathRatio.innerHTML = jsonResponse['data']['summary']['death_ratio'];
-    recoveryRatio.innerHTML = jsonResponse['data']['summary']['recovery_ratio'];
-    totalRecovered.innerHTML = jsonResponse['data']['summary']['recovered'];
-    tested.innerHTML = jsonResponse['data']['summary']['tested'];
-    console.log(jsonResponse['data']);
+  .then((jsonResponse) => {
+    const dataSummary = jsonResponse['data']['summary'];
+    activeCases.innerHTML = formatNumber(dataSummary['active_cases']);
+    totalCases.innerHTML = formatNumber(dataSummary['total_cases']);
+    critical.innerHTML = formatNumber(dataSummary['critical']);
+    totalDeaths.innerHTML = formatNumber(dataSummary['deaths']);
+    deathRatio.innerHTML = formatNumber(dataSummary['death_ratio']);
+    recoveryRatio.innerHTML = formatNumber(dataSummary['recovery_ratio']);
+    totalRecovered.innerHTML = formatNumber(dataSummary['recovered']);
+    tested.innerHTML = formatNumber(dataSummary['tested']);
   })
-  .catch(function(error){
-    console.log("Error: " + error)
+  .catch((error) => {
+    errorParagraph.innerText = error;
   });
 
+function formatNumber(number) {
+  return new Intl.NumberFormat().format(number);
+}
+
 searchBar.addEventListener('input', function(){});
-searchBtn.addEventListener('click', function(){});
+searchBtn.addEventListener('click', function(){
+  const searchValue = searchBar.value.trim();
+  console.log(searchValue);
+  searchBar.value = "";
+
+  // fetch(countryAPI, {
+  //   headers: {},
+  //   params: {}
+  // })
+});
+
+
+// var requestOptions = {
+//   method: 'GET',
+//   redirect: 'follow'
+// };
+
+// fetch("https://api.quarantine.country/api/v1/spots/summary", requestOptions)
+//   .then(response => response.text())
+//   // .then(result => console.log(result))
+//   .catch(error => console.log('error', error));

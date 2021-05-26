335c79425b63971ff7335506b1f007f6d2f617da
benjithorpe <benjaminthorpe19@gmail.com>
2021-05-26 11:21:22 +0000

    Add a json file which contains countries

--- main.js
+++ main.js
@@ -4,34 +4,26 @@ const totalDeaths = document.getElementById('deaths');
 const totalCases = document.getElementById('totalCases');
 const critical = document.getElementById('critical');
 const tested = document.getElementById('tested');
-const searchBtn = document.querySelector("button[type='search']");
-const searchBar = document.querySelector("input[type='search']");
 const deathRatio = document.getElementById('deathRatio');
 const recoveryRatio = document.getElementById('recoveryRatio');
 const totalRecovered = document.getElementById('recovered');
-let errorMessage = document.getElementById('error');
 const latestSummaryAPI = "https://api.quarantine.country/api/v1/summary/latest";
 let countryAPI = "https://api.quarantine.country/api/v1/summary/region?";
+const selectInput = document.getElementById('select');
+
+
+selectInput.addEventListener('input', () => {
+  const currentCountry = selectInput.value.trim();
+  updateDetailsFor(currentCountry);
+});
 
 fetch(latestSummaryAPI)
   .then((response) => response.json())
-  .then((jsonResponse) => {
-    const dataSummary = jsonResponse['data']['summary'];
-    updateDataWith(dataSummary);
-  })
-  .catch((error) => errorMessage.innerText = error);
+  .then((data) => updateDataWith(data['data']['summary']));
 
-searchBar.addEventListener('keypress', (event) => {
-  // code for the ENTER key is 13
-  if(event.keyCode == 13){
-    searchAndUpdateCountry();
-  }
-});
-
-searchBtn.addEventListener('click', () => searchAndUpdateCountry());
 const formatNumber = (number) => new Intl.NumberFormat().format(number);
 
-function updateDataWith(data){
+const updateDataWith = (data) => {
   activeCases.innerText = formatNumber(data['active_cases']);
   totalCases.innerText = formatNumber(data['total_cases']);
   critical.innerText = formatNumber(data['critical']);
@@ -42,25 +34,28 @@ function updateDataWith(data){
   tested.innerText = formatNumber(data['tested']);
 }
 
-function searchAndUpdateCountry(){
-  const searchValue = searchBar.value.trim();
-  searchBar.value = "";
-
-  if(!searchValue){
-    return;
-  }
-
-  fetch(countryAPI + `region=${searchValue}`)
+const updateDetailsFor = (country) => {
+  fetch(countryAPI + `region=${country}`)
     .then((response) => {
       if(response.ok){
-        errorMessage.innerText = "";
         return response.json();
       }
     })
     .then((data) => {
-      const regionSummary = data['data']['summary'];
-      countryName.innerText = searchValue;
-      updateDataWith(regionSummary);
-    })
-    .catch((error) => errorMessage.innerText = `Region / Country (${searchValue}) Does not Exists!!`);
+      countryName.innerText = country;
+      updateDataWith(data['data']['summary']);
+    });
 }
+
+function loadJSON(){
+  const values = JSON.parse(countries);
+
+  values.forEach((country) => {
+    const optionTag = document.createElement('option');
+    optionTag.setAttribute("value", `${country['name'].toLowerCase()}`);
+    optionTag.innerText = `${country['name']}`;
+    selectInput.appendChild(optionTag);
+  });
+}
+
+loadJSON();
